e562f37bbf0e9ecbb15a4813bf2e675eb27d79d5
Nithwin <vmnithwin@gmail.com>
2026-07-04 23:14:05 +0530

    feat: integrate Gemini LLM and stateful memory persistence into LangGraph chatbot

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 from typing_extensions import TypedDict
+from typing import Annotated
+from langgraph.graph.message import add_messages
 from langgraph.graph import StateGraph, START, END
+from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.checkpoint.memory import MemorySaver
+import os
 
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    google_api_key=os.getenv("GEMINI_API_KEY"),
+)
+
+memory = MemorySaver()
 
 class State(TypedDict):
-    question: str
-    answer: str
+    messages: Annotated[list, add_messages]
 
 def chatbot(state: State):
+    response = llm.invoke(state["messages"])
+
     return {
-        "answer": f"You asked: {state['question']}"
+        "messages": [response]
     }
 
 graph_builder = StateGraph(State)
@@ -28,14 +44,28 @@ graph_builder.add_edge(
     END,
 )
 
-graph = graph_builder.compile()
+graph = graph_builder.compile(
+    checkpointer=memory
+)
+
+config = {
+    "configurable": {
+        "thread_id": "1"
+    }
+}
 
 result = graph.invoke({
-    "question": "What is LangGraph?"
-})
+    "messages": [
+        HumanMessage(content="Hello i'm Nithwin!")
+    ]
+},
+config=config,)
 
 result = graph.invoke({
-    "question": "Where is LangGraph used?"
-})
+    "messages": [
+        HumanMessage(content="What is my name?")
+    ]
+},
+config=config,)
 
 print(result)
